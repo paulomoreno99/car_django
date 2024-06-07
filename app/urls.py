aac5e4b399532ea300fc4e8fbd9d95814dcb0255
paulomoreno99 <paulom.info@gmail.com>
2024-06-07 10:58:55 -0300

    views_example_aula42

--- app/urls.py
+++ app/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+def car_view(request):
+    return HttpResponse('<h1>Paulo Moreno Desenvolvedor Python</h1>')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('cars/', car_view)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Brand, Car
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
@@ -7,5 +7,8 @@ class CarAdmin(admin.ModelAdmin):
 
 admin.site.register(Car, CarAdmin)
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
-
+admin.site.register(Brand, BrandAdmin)
